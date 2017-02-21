ae0c1f0819975d36935f7dbff7ebce424b294e7f
Leon.Nie <leonnie52@163.com>
2017-02-21 15:59:04 +0800

    Demo Script.

--- MyScript/Demo_single.py
+++ MyScript/Demo_single.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+import logging
 from dronekit import connect,VehicleMode,LocationGlobalRelative
 from takeoff import *
+
+from drone_network import Networking
+from collision_avoidance import CollisionThread
 
 connection_string = 'tcp:127.0.0.1:5763'
 
@@ -10,11 +14,25 @@ connection_string = 'tcp:127.0.0.1:5763'
 print 'Connecting to vehicle1 on: %s' % connection_string
 vehicle = connect(connection_string, wait_ready=True)
 
+# Create the interface with UDP broadcast sockets
+address = ("192.168.6.255", 54545)
+network = Networking(address, "UDP_BROADCAST", vehicle)
+
+# Add collision avoidance algorithm
+t_collision = CollisionThread(network)
+
 
 # Get all vehicle attributes (state)
 print "\nGet all vehicle attribute values:"
 print " Autopilot Firmware version: %s" % vehicle.version
 print "System ID：%s" % vehicle.parameters['SYSID_THISMAV']
+
+
+logging.info("Initializing interface")
+network.run()
+
+logging.info("Starting collision avoidance scheme")
+t_collision.start()
 
 time.sleep(10)
 # Close vehicle object before exiting script
