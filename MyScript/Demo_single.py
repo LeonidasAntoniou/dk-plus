4442207861ce62368fe44fc100c4654b0ae8eca2
Leon.Nie <leonnie52@163.com>
2017-02-21 16:38:12 +0800

    debug mode.

--- MyScript/Demo_single.py
+++ MyScript/Demo_single.py
@@ -2,10 +2,11 @@
 # -*- coding:utf-8 -*-
 
 import logging
-from dronekit import connect,VehicleMode,LocationGlobalRelative
+from dronekit import connect, VehicleMode, LocationGlobalRelative
 from takeoff import *
 
 import sys
+
 sys.path.append("..")
 
 from drone_network import Networking
@@ -18,18 +19,17 @@ print 'Connecting to vehicle1 on: %s' % connection_string
 vehicle = connect(connection_string, wait_ready=True)
 
 # Create the interface with UDP broadcast sockets
+debug = True
 address = ("192.168.6.255", 54545)
-network = Networking(address, "UDP_BROADCAST", vehicle)
+network = Networking(address, "UDP_BROADCAST", vehicle, debug)
 
 # Add collision avoidance algorithm
 t_collision = CollisionThread(network)
-
 
 # Get all vehicle attributes (state)
 print "\nGet all vehicle attribute values:"
 print " Autopilot Firmware version: %s" % vehicle.version
 print "System ID：%s" % vehicle.parameters['SYSID_THISMAV']
-
 
 logging.info("Initializing interface")
 network.run()
