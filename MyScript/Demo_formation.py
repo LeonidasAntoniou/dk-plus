b5bea47ac7d896c2cd293e97e2148dd5371cbde1
Leon.Nie <leonnie52@163.com>
2017-03-10 21:59:08 +0800

    Position in formation bug fixed.

--- MyScript/Demo_formation.py
+++ MyScript/Demo_formation.py
@@ -71,9 +71,9 @@ print "System ID：%s" % vehicle.parameters['SYSID_THISMAV']
 # Set the targetLocation for the team, Heading South
 # t_collision.formation.set_target_Loc(lat=39.979352, lon=116.339748, alt=10, dNorth=-90, dEast=0)
 # Heading East
-t_collision.formation.set_target_Loc(lat=39.979352, lon=116.345748, alt=10, dNorth=-90, dEast=0)
+t_collision.formation.set_target_Loc(lat=39.979352, lon=116.339748, alt=10, dNorth=0, dEast=100)
 # Haading West
-# t_collision.formation.set_target_Loc(lat=39.979352, lon=116.289748, alt=10, dNorth=-90, dEast=0)
+# t_collision.formation.set_target_Loc(lat=39.979352, lon=116.339748, alt=10, dNorth=-90, dEast=-100)
 # t_collision.formation.setFormation(np.array([[0, 0, 0],
 #                                              [20.0, 0, -20.0],
 #                                              [0, 0, 0]]))
